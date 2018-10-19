81624f0d020bccfcdb9e47355ba0f0da0bc4e720
jaxycocoa <43188397+jaxycocoa@users.noreply.github.com>
2018-10-19 13:21:48 -0700

    added player movement script

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ class PlayerController : MonoBehaviour {
+    private Rigidbody m_rb;
+	// Use this for initialization
+	void Start () {
+        m_rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void FixedUpdate()
+    {
+        // get user input to add left right force to our player object
+
+        float movement = Input.GetAxis("Horizontal");
+
+        m_rb.AddForce(new Vector3(movement, 0.0F, 0.0F));
+
+    }
+}
